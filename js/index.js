cdad2c4763014775ea699ec117addfccaaf997ac
Johnson Chen <JohnsonChen12345@gmail.com>
2024-10-06 19:14:18 -0400

    carasoul, icons, direction

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
+const socialIcons = document.getElementById('social-icons');
+const directionButton = document.getElementById('direction-button');
 
 function toggleSidebar(){
   sidebar.classList.toggle('close')
   toggleButton.classList.toggle('rotate')
+  socialIcons.classList.toggle('hide');
+  directionButton.classList.toggle('invisible');
 }
 
